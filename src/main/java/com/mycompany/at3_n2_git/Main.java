6e3c66ea3e0594419e920fa87c4b052b5b5b631a
caiosantosbsb <caiosantosbsb@gmail.com>
2024-06-17 11:10:17 -0300

    Primeiro commit, iniciando projeto

---
+++ src/main/java/com/mycompany/at3_n2_git/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.at3_n2_git;
+
+/**
+ *
+ * @author caiov
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
